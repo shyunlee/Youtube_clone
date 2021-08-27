ce44288b0b6b4253d3f49679aa4f1c0f3327e124
SEAN LEE <seanlee@MacBook-Pro.local>
2021-08-26 22:31:48 -0500

    update images route

--- src/components/search_header/SearchHeader.jsx
+++ src/components/search_header/SearchHeader.jsx
@@ -25,12 +25,12 @@ const SearchHeader = memo(({onSearch}) => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img className={styles.logo_img} src="/images/logo.png" alt="logo"/>
+        <img className={styles.logo_img} src="./images/logo.png" alt="logo"/>
         <h1 className={styles.logo_title}>Sean Youtube</h1>
       </div>
         <input ref={inputRef} className={styles.input} type="search" placeholder="Search.." onKeyPress={pressHandle}/>
         <button className={styles.search_btn} type="submit" onClick={clickHandle}>
-          <img className={styles.search_img} src="/images/search.png" alt="search" />
+          <img className={styles.search_img} src="./images/search.png" alt="search" />
         </button>
     </header>
   )
